7c4fcbbb6ac3711ae71d4c4e7380ddca51cbcee9
Lionel Wijaya <lionelchris1509@gmail.com>
2024-08-02 20:43:48 +0700

    add attacks

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,6 +15,8 @@ var anim_player: AnimationPlayer
 var state_timer = 0
 var combo_progress = 0
 
+var stepDistance = 0
+
 var input_buffer = ""
 
 func _ready():
@@ -33,25 +35,42 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	# handle state
-	if state == STATE.ATTACKING && state_timer > COMBO_LENGTH[combo_progress]:
-		state_timer = 0
-		anim_player.play("slash" + combo_progress)
-		combo_progress += 1
+	if state == STATE.ATTACKING:
+		if combo_progress >= COMBO_LENGTH.size():
+			combo_progress = 0
+		if state_timer > COMBO_LENGTH[combo_progress]:
+			state = STATE.RECOVERING
+	else:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if stepDistance > 0:
+		velocity.x = stepDistance / delta
+		stepDistance = 0
 
 	move_and_slide()
-	
+
+	if state == STATE.ATTACKING:
+		velocity.x = 0
+
 func _unhandled_key_input(event):
 	var can_override_state = true;
-	can_override_state &= state == STATE.ATTACKING && state_timer < COMBO_LENGTH[combo_progress]
+	can_override_state = can_override_state && !(state == STATE.ATTACKING && state_timer < COMBO_LENGTH[combo_progress])
 	
 	if can_override_state:
 		if event.is_action_pressed("basic_attack"):
+			if state == STATE.RECOVERING:
+				combo_progress += 1
 			state = STATE.ATTACKING
+			state_timer = 0
+			anim_player.play("slash1")
 		if event.is_action_pressed("dodge"):
 			state = STATE.DODGING
+			state_timer = 0
+
+func step(distance):
+	stepDistance = distance
